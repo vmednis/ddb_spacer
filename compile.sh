35b7e57606f003857810ee818227a510c2abc5db
Valters Mednis <valters.mednis@gmail.com>
2016-12-11 22:33:35 +0200

    Added existing source

---
+++ compile.sh
@@ -0,0 +1,7 @@
+echo "Cleaning up!"
+rm spacer.o
+rm ddb_spacer.so
+echo "Compiling!"
+gcc -Wall -g -fPIC -std=c99 -D_GNU_SOURCE -I/opt/deadbeef/include `pkg-config --cflags gtk+-3.0` spacer.c -c -o spacer.o
+echo "Linking!"
+gcc -shared -g spacer.o `pkg-config --libs gtk+-3.0` -o ddb_spacer.so
---
+++ spacer.c
@@ -0,0 +1,239 @@
+/*
+		Copyright (c) 2016 Valters Mednis
+
+		Permission is hereby granted, free of charge, to any person obtaining a
+		copy of this software and associated documentation files (the "Software"),
+		to deal in the Software without restriction, including without limitation
+		the rights to use, copy, modify, merge, publish, distribute, sublicense,
+		and/or sell copies of the Software, and to permit persons to whom the
+		Software is furnished to do so, subject to the following conditions:
+
+		The above copyright notice and this permission notice shall be included in
+		all copies or substantial portions of the Software.
+
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+		FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+		DEALINGS IN THE SOFTWARE.
+*/
+
+#include <stdlib.h>
+#include <string.h>
+#include <gtk/gtk.h>
+
+#include </opt/deadbeef/include/deadbeef/deadbeef.h>
+#include </opt/deadbeef/include/deadbeef/gtkui_api.h>
+
+
+#define SPACER_WIDGET_TYPE "spacer"
+
+static DB_functions_t *deadbeef;
+static ddb_gtkui_t *gtkui_plugin;
+
+
+static void
+print(const char* str) {
+	printf("\e[1;32mSpacer: %s\e[0m\n", str);
+}
+
+typedef struct {
+	ddb_gtkui_widget_t base;
+	int width;
+	int height;
+} w_spacer_t;
+
+static void
+w_spacer_set_size(w_spacer_t * widget, int width, int height) {
+	widget->width = width;
+	widget->height = height;
+
+	gtk_widget_set_size_request(widget->base.widget, widget->width, widget->height);
+}
+
+static void
+w_spacer_save(ddb_gtkui_widget_t *w, char *s, int sz) {
+	w_spacer_t * widget = (w_spacer_t *) w;
+
+	//Generate save data
+	char savedata[25];
+
+	sprintf(savedata, " %i %i", widget->width, widget->height);
+
+	//Append save data
+	strncat(s, savedata, sz);
+}
+
+const char *
+w_spacer_load(ddb_gtkui_widget_t *w, const char *type, const char *s) {
+	w_spacer_t * widget = (w_spacer_t *) w;
+
+	print(s);
+
+	int width, height;
+
+	//Do the reading magic
+	sscanf(s, " %i %i", &width, &height);
+
+	w_spacer_set_size(widget, width, height);
+
+	//return a pointer the way api wants it back
+	return strchr(s, '{') + 1;
+}
+
+typedef struct {
+	w_spacer_t * widget;
+	GtkWidget * widthentry;
+	GtkWidget * heightentry;
+} w_spacer_menu_changesize_dialog_userdata_t;
+
+static void
+w_spacer_menu_changesize_dialog(GtkWidget * dialog, int response, void * userdata) {
+	print("Dialog action performed");
+	w_spacer_menu_changesize_dialog_userdata_t * data = userdata;
+	w_spacer_t * widget = data->widget;
+	GtkWidget * widthentry = data->widthentry;
+	GtkWidget * heightentry = data->heightentry;
+
+	int width = gtk_spin_button_get_value_as_int((GtkSpinButton *) widthentry);
+	int height = gtk_spin_button_get_value_as_int((GtkSpinButton *) heightentry);
+
+	switch (response) {
+		case GTK_RESPONSE_APPLY: {
+			w_spacer_set_size(widget, width, height);
+		} break;
+		case GTK_RESPONSE_OK: {
+			w_spacer_set_size(widget, width, height);
+			gtk_widget_destroy(dialog);
+		} break;
+		default: {
+			gtk_widget_destroy(dialog);
+		} break;
+	}
+}
+
+static void
+w_spacer_menu_changesize(GtkWidget * menuitem, void * userdata) {
+	print("Menu item activated");
+	w_spacer_t * widget = userdata;
+
+	//Build the dialog
+	GtkWidget * dialog = gtk_dialog_new_with_buttons("Change spacer size", (GtkWindow *) gtkui_plugin->get_mainwin(),
+																									 GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
+																								   "Apply", GTK_RESPONSE_APPLY, "Cancel", GTK_RESPONSE_CANCEL, "OK", GTK_RESPONSE_OK,
+																								 	 NULL);
+	GtkWidget * dialogcontent = gtk_dialog_get_content_area((GtkDialog *) dialog);
+	gtk_orientable_set_orientation((GtkOrientable *) dialogcontent, GTK_ORIENTATION_VERTICAL);
+
+	GtkWidget * widthbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+	GtkWidget * heightbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+	gtk_container_add(GTK_CONTAINER(dialogcontent), widthbox);
+	gtk_container_add(GTK_CONTAINER(dialogcontent), heightbox);
+	gtk_box_set_child_packing((GtkBox *) dialogcontent, widthbox, TRUE, TRUE, 0, GTK_PACK_START);
+	gtk_box_set_child_packing((GtkBox *) dialogcontent, heightbox, TRUE, TRUE, 0, GTK_PACK_END);
+
+	GtkWidget * widthlabel = gtk_label_new("Width:");
+	GtkWidget * widthentry = gtk_spin_button_new_with_range((double) 0, (double) (8192), (double) 1);
+	gtk_container_add(GTK_CONTAINER(widthbox), widthlabel);
+	gtk_container_add(GTK_CONTAINER(widthbox), widthentry);
+	gtk_box_set_child_packing((GtkBox *) widthbox, widthlabel, TRUE, TRUE, 8, GTK_PACK_START);
+	gtk_box_set_child_packing((GtkBox *) widthbox, widthentry, FALSE, FALSE, 8, GTK_PACK_START);
+	gtk_label_set_justify((GtkLabel *) widthlabel, GTK_JUSTIFY_LEFT);
+
+
+	GtkWidget * heightlabel = gtk_label_new("Height:");
+	GtkWidget * heightentry = gtk_spin_button_new_with_range((double) 0, (double) (8192), (double) 1);
+	gtk_container_add(GTK_CONTAINER(heightbox), heightlabel);
+	gtk_container_add(GTK_CONTAINER(heightbox), heightentry);
+	gtk_box_set_child_packing((GtkBox *) heightbox, heightlabel, TRUE, TRUE, 8, GTK_PACK_START);
+	gtk_box_set_child_packing((GtkBox *) heightbox, heightentry, FALSE, FALSE, 8, GTK_PACK_START);
+	gtk_label_set_justify((GtkLabel *) heightlabel, GTK_JUSTIFY_LEFT);
+
+	gtk_spin_button_set_value((GtkSpinButton *) widthentry, (double) widget->width);
+	gtk_spin_button_set_value((GtkSpinButton *) heightentry, (double) widget->height);
+
+	gtk_widget_show(dialog);
+	gtk_widget_show_all(dialogcontent);
+
+	//Register the event listenr
+	w_spacer_menu_changesize_dialog_userdata_t * data = malloc(sizeof(w_spacer_menu_changesize_dialog_userdata_t));
+	data->widget = widget;
+	data->widthentry = widthentry;
+	data->heightentry = heightentry;
+	g_signal_connect_after((void *) dialog, "response", G_CALLBACK(w_spacer_menu_changesize_dialog), data);
+}
+
+static void
+w_spacer_initmenu(ddb_gtkui_widget_t * widget, GtkWidget *menu) {
+	GtkWidget * menuitem = gtk_menu_item_new_with_mnemonic("Change spacer size");
+	gtk_widget_show(menuitem);
+	gtk_container_add(GTK_CONTAINER(menu), menuitem);
+
+	//Register the event handler
+	g_signal_connect_after((void *) menuitem, "activate", G_CALLBACK(w_spacer_menu_changesize), widget);
+}
+
+static ddb_gtkui_widget_t *
+w_spacer_create(void) {
+	print("Creating widget");
+	w_spacer_t * widget = malloc(sizeof(w_spacer_t));
+	memset(widget, 0, sizeof(w_spacer_t));
+
+	widget->base.widget = (GtkWidget *) gtk_event_box_new();
+  widget->base.initmenu = w_spacer_initmenu;
+	widget->base.save = w_spacer_save;
+	widget->base.load = w_spacer_load;
+
+	//The default spacer size
+	w_spacer_set_size(widget, 64, 64);
+
+	//Needed so that the design mode would work
+	gtkui_plugin->w_override_signals(widget->base.widget, widget);
+
+	return (ddb_gtkui_widget_t *) widget;
+}
+
+static int
+spacer_connect (void) {
+	print("Connect was called");
+  gtkui_plugin = (ddb_gtkui_t *)deadbeef->plug_get_for_id(DDB_GTKUI_PLUGIN_ID);
+  //If GTK UI plugin isn't loaded
+  if(!gtkui_plugin) {
+  	return -1;
+  }
+  gtkui_plugin->w_reg_widget("Spacer", 0, w_spacer_create, SPACER_WIDGET_TYPE, NULL);
+	return 0;
+}
+
+static int
+spacer_disconnect (void) {
+    gtkui_plugin->w_unreg_widget(SPACER_WIDGET_TYPE);
+	return 0;
+}
+
+DB_misc_t plugin = {
+    .plugin.api_vmajor = DB_API_VERSION_MAJOR,
+    .plugin.api_vminor = DB_API_VERSION_MINOR,
+    .plugin.type = DB_PLUGIN_MISC,
+    .plugin.version_major = 1,
+    .plugin.version_minor = 0,
+    .plugin.id = "spacer",
+    .plugin.name ="Spacer",
+    .plugin.descr = "Provides GUI element that acts like a spacer so you can create that perfect layout.\n" \
+                    "The size of the spacer can be changed in design mode.\n",
+    .plugin.copyright = "Copyright (c) 2016 Valters Mednis\n\n" \
+												"Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\n" \
+												"The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\n"
+												"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.",
+    .plugin.connect = spacer_connect,
+    .plugin.disconnect = spacer_disconnect
+};
+
+extern DB_plugin_t *
+ddb_spacer_load (DB_functions_t *ddb) {
+		print("Load was called");
+    deadbeef = ddb;
+    return DB_PLUGIN(&plugin);
+}
